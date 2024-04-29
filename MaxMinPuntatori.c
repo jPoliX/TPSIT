e18e422985b95751bf35a713323f4c61f389aeda
jPoliX <150335994+jPoliX@users.noreply.github.com>
2024-04-29 17:10:15 +0200

    Create MaxMinPuntatori.c

---
+++ MaxMinPuntatori.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+   int quantiNumeri=0, n=0,max=0,min=0;
+   float media=0,somma=0;
+printf("inserisci la dimensione del vettore\n");
+scanf("%d",&quantiNumeri);
+
+float vettore[quantiNumeri];
+   float*puntatore=vettore;
+printf("inserisci i numeri per il vettore\n");
+
+for(int i=0;i<quantiNumeri;i++){ 
+scanf("%f",puntatore+i);
+}
+
+ min = max = *puntatore;
+for(int i=0;i<quantiNumeri;i++){
+    somma+=*(puntatore+i);
+    if (*(puntatore + i) < min){
+            min = *(puntatore + i);
+}
+        if (*(puntatore + i) > max){
+            max = *(puntatore + i);
+  }
+}
+    media=somma/quantiNumeri;
+    
+printf("la media è: %f\n",media);
+printf("il minimo è: %d\n",min);
+printf("il massimo è: %d\n",max);
+
+    return 0;
+}
