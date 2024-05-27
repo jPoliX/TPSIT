e10e588e54e977c0d599bec414aa8048cbd067c3
jPoliX <150335994+jPoliX@users.noreply.github.com>
2024-05-27 10:04:12 +0200

    Create xNumeri.c

---
+++ xNumeri.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int x=0,dimensione=0,n=0;
+   printf("inserisci la dimensione: ");
+   scanf("%d",&dimensione);
+   
+   int array[dimensione];
+   
+ 
+   for(int i=0;i<dimensione;i++){
+       printf("inserisci un numero: ");
+       scanf("%d",&n);
+       array[i]=n;
+   }
+   printf("inserisci la quantità di numeri che vuoi visualizzare: ");
+   scanf("%d",&x);
+   
+    for(int i = 0; i < x; i++) {
+      int min = i;
+      for (int j = i + 1; j < x; j++) {
+         if (array[j] < array[min])
+            min = j;
+      }
+      int t = array[min];
+      array[min] = array[i];
+      array[i] = t;
+    }
+
+   for(int i = 0; i < x; i++) {
+      printf("%d ", array[i]);      
+   }   
+
+
+    return 0;
+}
