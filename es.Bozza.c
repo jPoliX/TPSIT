c9e423543736b84134faf492266e4f755a1db6a6
jPoliX <150335994+jPoliX@users.noreply.github.com>
2024-05-28 15:32:56 +0200

    Update es.Bozza.c

--- es.Bozza.c
+++ es.Bozza.c
@@ -1,6 +1,6 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
     int dimensione=0;
@@ -9,15 +9,17 @@ int main() {
     printf("Inserisci la dimensione: ");
     scanf("%d",&dimensione);
     int arr[dimensione]; 
+    
+    srand(time(NULL)); //variabile per generare numeri diversi ogni esecuzione
   
     for (int i = 0; i < dimensione; i++) {
-        arr[i] = rand() % 10;
+        arr[i] = rand() % 10; //genera numeri random da 1 a 10
     }
       for(int i=0;i<dimensione;i++){
         somma+=arr[i];
     }
     
-    for(int i = 0; i < dimensione; i++) {
+    for(int i = 0; i < dimensione; i++) { // riordino l'array usando il selection sort
       int min = i;
       for (int j = i + 1; j < dimensione; j++) {
          if (arr[j] < arr[min])
@@ -27,19 +29,21 @@ int main() {
       arr[min] = arr[i];
       arr[i] = t;
     }
-    
-    int arr2[dimensione];
-    int indice=0;
-    for(int i=1;i<dimensione;i++){
-        if(arr[i]!=arr[i-1]){
-            arr2[indice++]=arr[i];
+  
+    int indice = 0;
+    int arr2[dimensione]; // creo l'array eliminando i duplicati
+    arr2[indice++] = arr[0]; // metto il primo elemento dell'array con duplicati al array senza duplicati
+    for (int i = 1; i < dimensione; i++) {
+        if (arr[i] != arr[i - 1]) { //se l'elemento in posizione i è diverso da quello nella posizione precendente lo aggiungo nel nuovo array
+            arr2[indice++] = arr[i];
         }
     }
+    
       for(int i=0;i<indice;i++){
         somma2+=arr2[i];
     }
    
-    media=(float)somma/dimensione;
+    media=(float)somma/dimensione; //trovo le medie dei 2 array
     media2=(float)somma2/indice;
     
     for (int i = 0; i < dimensione; i++) {
@@ -47,17 +51,18 @@ int main() {
     }
     printf("\n");
 
-    for(int i=0;i<dimensione;i++){
-        printf("%d",arr2[i]);
-    }
+   
     printf("\n");
     printf("Media duplicati: %.2f\n",media);
     printf("Media unici: %.2f",media2);
     printf("\n");
     
-    if(somma>somma2){
-        printf("La media con i duplicati è maggiore");
-    }else printf("La media senza duplicati è maggiore");
+   if (media > media2) { // confronto le due medie
+        printf("La media con i duplicati è maggiore\n");
+    } else if(media==media2){
+        printf("La due medie hanno lo stesso valore\n");
+    }else 
+    printf("La media senza duplicati è maggiore")
  
     return 0;
 }
