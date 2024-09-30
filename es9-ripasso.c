4a39ce62c7d4db4c0daf856eb759e7db4f5c5cd3
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-09-30 18:43:12 +0200

    Update es9-ripasso.c

--- es9-ripasso.c
+++ es9-ripasso.c
@@ -1,5 +1,5 @@
-/*Progettare un algoritmo che risolva il seguente problema. Si richieda 
-all’utente di inserire una serie di terne di dati numerici (A, B, C). 
+/*Progettare un algoritmo che risolva il seguente problema. Si richieda
+all’utente di inserire una serie di terne di dati numerici (A, B, C).
 Il programma deve terminare quando uno dei valori inseriti è minore di
 zero. Si scartino le terne nelle quali i valori non sono in ordine
 strettamente crescente, ovvero quelle terne per cui non valga A < B < C.
@@ -11,32 +11,36 @@ di terminare il programma.*/
 
 int main()
 {
-    int n1, n2, n3, max, min;
-    do{
-     printf("Inserisci il primo numero: \n");
+    int n1, n2, n3, max = 0, min = 0;
+    do
+    {
+        printf("Inserisci il primo numero: \n");
         scanf("%d", &n1);
         printf("Inserisci il secondo numero: \n");
         scanf("%d", &n2);
         printf("Inserisci il terzo numero: \n");
         scanf("%d", &n3);
-        if(n1<n2<n3){
-            if(n1<n2 && n1<n3){
-                min=n1;
-            } else if(n2<n1 && n2<n3){
-                min=n2;
-            } else min=n3;
-            
-            if(n1>n2 && n1>n3){
-                max = n1;
-            } else if(n2>n1 && n2>n3){
-                max = n2;
-            }else max = n3;
-        }else 
-        printf("numeri non in ordine\n");
-    }while(n1>=0 && n2>=0 && n3>=0);
-    
-    printf("valore minimo: %d\n",min);
-    printf("valore massimo: %d\n",max);
-    
+
+        if (n1 < n2 && n2 < n3)
+        {
+            if (n1 < min || min == 0)
+            {
+                min = n1;
+            }
+            if (n3 > max)
+            {
+                max = n3;
+            }
+        }
+        else
+        {
+            printf("Numeri non in ordine crescente \n");
+        }
+
+    } while (n1 >= 0 && n2 >= 0 && n3 >= 0);
+
+    printf("valore minimo: %d\n", min);
+    printf("valore massimo: %d\n", max);
+
     return 0;
 }
