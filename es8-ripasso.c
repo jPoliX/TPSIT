2d44dcd8e76203df7fd64fdef52523e9e2fb9078
alessiopoli <150335994+jPoliX@users.noreply.github.com>
2024-09-30 17:34:57 +0200

    Update es8-ripasso.c

--- es8-ripasso.c
+++ es8-ripasso.c
@@ -1,6 +1,6 @@
-/*Progettare un algoritmo che risolve il seguente problema. Si desidera 
-calcolare la somma delle radici quadrate di N valori numerici inseriti 
-dall’utente, con N inserito in input. 
+/*Progettare un algoritmo che risolve il seguente problema. Si desidera
+calcolare la somma delle radici quadrate di N valori numerici inseriti
+dall’utente, con N inserito in input.
 L’algoritmo deve stampare la somma calcolata. L’algoritmo deve terminare
 con un messaggio di errore quando viene inserito un numero che non
 permette di effettuare il calcolo (nel dominio dei numeri reali).*/
@@ -10,18 +10,25 @@ permette di effettuare il calcolo (nel dominio dei numeri reali).*/
 
 int main()
 {
-    int N, numero, radice=0;
-    float somma=0;
+    int N, numero;
+    float somma = 0, radice = 0;
     printf("Inserisci quanti numeri deve calcolare: ");
     scanf("%d", &N);
-    while(N>0){
+    while (N > 0)
+    {
         printf("Inserisci un numero: ");
         scanf("%d", &numero);
-        radice=sqrt(numero);
-        somma+=radice;
-        N--;
+        radice = sqrt(numero);
+        if (numero >= 0)
+        {
+            somma += radice;
+            N--;
+        }
+        else
+            printf("radice impossibile \n");
     }
+
     printf("somma radici quadrate: %.2f", somma);
-    
+
     return 0;
 }
